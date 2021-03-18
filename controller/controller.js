eca9d16c3c2ce814dd9887cd69f39aa8cce61ca4
gusfel <agustinjfeliciano@gmail.com>
2021-03-17 17:31:37 -0700

    working results from get questions before i refactor

--- controller/controller.js
+++ controller/controller.js
@@ -20,76 +20,30 @@ module.exports.getAnswers = (product_id, callback) => {
     } else {
       const answers = data.rows;
       callback(null, answers);
-      // const reviewList =
-      // callback(null, data);
     }
   });
 };
 
-module.exports.getAll = (product_id, callback) => {
+module.exports.getQuestions = (product_id, callback) => {
   const query = `SELECT * FROM questions WHERE product_id = ${product_id} limit 10`;
   pool.query(query, (err, qData) => {
     if (err) {
       console.log('sorry');
     } else {
       const questions = qData.rows;
-      // console.log(qData.rows);
       callback(null, questions);
-
-      // Promise.all(
-      //   questions.map((question) => {
-      //     const { question_id } = question;
-      //     const qWithA = question;
-      //     const res = getAnswers(question_id, (aErr, aData) => {
-      //       if (aErr) {
-      //         console.log('no answers');
-      //       }
-      //       const answers = aData.rows;
-      //       const answersObj = {};
-      //       answers.forEach((answer) => {
-      //         answersObj[answer.id] = answer;
-      //       });
-      //       qWithA.answers = answersObj;
-      //       return qWithA;
-      //     });
-      //     return qWithA
-      //   }),
-      // )
-      //   .then((response) => {
-      //     console.log(response);
-      //   });
     }
   });
 };
 
-module.exports.getAPhotos = (answer_id, callback) => {
-  const query = `SELECT * FROM photos WHERE answer_id = ${answer_id} limit 5`;
+module.exports.getPhotos = (product_id, callback) => {
+  const query = `SELECT * FROM photos WHERE answer_id in (SELECT id FROM answers WHERE question_id in (SELECT question_id from questions WHERE product_id = ${product_id} limit 10) limit 10) limit 5`;
   pool.query(query, (err, data) => {
     if (err) {
       console.log('sorry');
     } else {
-      const questions = data.rows;
-      console.log(data.rows);
-      // const reviewList =
-      // callback(null, data);
+      const photos = data.rows;
+      callback(null, photos);
     }
   });
 };
-
-// questions.forEach((question) => {
-//   const { question_id } = question;
-//   const qWithA = question;
-//   getAnswers(question_id, (aErr, aData) => {
-//     if (aErr) {
-//       console.log('no answers');
-//     } else {
-//       const answers = aData.rows;
-//       const answersObj = {};
-//       answers.forEach((answer) => {
-//         answersObj[answer.id] = answer;
-//       });
-//       qWithA.answers = answersObj;
-//       return qWithA;
-//     }
-//   });
-// });
--- server.js
+++ server.js
@@ -4,40 +4,55 @@ const controller = require('./controller/controller.js');
 const app = express();
 const port = 2000;
 
-app.use(express.urlencoded({ extended: true }));
-
-// app.get('/', (req, res) => {
-//   res.send('Hello World!');
-// });
+// app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
 
 app.get('/qa/questions', (req, res) => {
-  // console.log(req);
   const { product_id } = req.query;
-  controller.getAll(product_id, (err, data) => {
+  controller.getQuestions(product_id, (err, data) => {
     if (err) {
       res.status(404);
     } else {
-      // res.send(data);
-      const questions = data;
-      controller.getAnswers(product_id, (aErr, aData) => {
-        if (aErr) {
-          console.log('answer server err');
+      controller.getPhotos(product_id, (pErr, pData) => {
+        if (pErr) {
+          console.log('photo server error');
         } else {
-          const answers = aData;
-          const answersObj = {};
-          // console.log(answers);
-          answers.forEach((answer) => {
-            answersObj[answer.question_id] = {
-              [answer.id]: answer,
-            };
+          const photoObj = {};
+          pData.forEach((photo) => {
+            if (photoObj[photo.answer_id]) {
+              photoObj[photo.answer_id].push(photo.photo_url);
+            } else {
+              photoObj[photo.answer_id] = [photo.photo_url];
+            }
           });
-          // Object.keys(questions)
-          console.log(questions);
-          questions.map(question => {
-            question.answers = answersObj[question.question_id];
-            return question;
+          const questions = data;
+          controller.getAnswers(product_id, (aErr, aData) => {
+            if (aErr) {
+              console.log('answer server err');
+            } else {
+              const answers = aData;
+              const answersObj = {};
+              answers.forEach((answer) => {
+                if (photoObj[answer.id]) {
+                  answer.photos = photoObj[answer.id];
+                } else {
+                  answer.photos = [];
+                }
+                answersObj[answer.question_id] = {
+                  [answer.id]: answer,
+                };
+              });
+              questions.map((question) => {
+                question.answers = answersObj[question.question_id];
+                return question;
+              });
+              const sendObj = {
+                product_id,
+                results: questions,
+              };
+              res.send(sendObj);
+            }
           });
-          console.log(questions);
         }
       });
     }
