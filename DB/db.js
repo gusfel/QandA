343fea802df0cc2675391efcd36f9b84a2d52f6a
gusfel <agustinjfeliciano@gmail.com>
2021-03-27 10:59:42 -0700

    changing pool back to online db

--- DB/db.js
+++ DB/db.js
@@ -1,26 +1,26 @@
 const { Pool } = require('pg');
 
-// const db = new Pool({
-//   user: 'postgres',
-//   host: 'ec2-3-22-225-72.us-east-2.compute.amazonaws.com',
-//   database: 'qanda',
-//   password: 'postgres',
-//   port: 5432,
-//   max: 100,
-//   idleTImeoutMillis: 30000,
-//   connectionTImeoutMillis: 4000,
-// });
-
 const db = new Pool({
   user: 'postgres',
-  host: 'localhost',
-  database: 'postgres',
+  host: 'ec2-3-22-225-72.us-east-2.compute.amazonaws.com',
+  database: 'qanda',
   password: 'postgres',
   port: 5432,
   max: 100,
   idleTImeoutMillis: 30000,
   connectionTImeoutMillis: 4000,
 });
+
+// const db = new Pool({
+//   user: 'postgres',
+//   host: 'localhost',
+//   database: 'postgres',
+//   password: 'postgres',
+//   port: 5432,
+//   max: 100,
+//   idleTImeoutMillis: 30000,
+//   connectionTImeoutMillis: 4000,
+// });
 
 // db.connect();
 
